254dd57e8e0229ead5bf4f3f66e8c5b9d7793c4e
Aviva Ruben <aviva@rubenfamily.com>
2025-03-24 14:54:40 -0500

    rayon

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::{fs::File, io::Write};
 
+use rayon::prelude::*;
+
 use translate::translate;
 
 mod translate;
 
 fn main() {
     let src = include_str!("prospero.vm");
-    let dim: usize = 256;
+    let dim: usize = 1024;
 
     // this might be especially evil...
     let step = 2f64 / dim as f64;
@@ -16,6 +18,8 @@ fn main() {
         .flat_map(|y_step| (0..dim).rev().map(move |x_step| (x_step, y_step)))
         .rev()
         .map(|(x_step, y_step)| (-1f64 + step * x_step as f64, -1f64 + step * y_step as f64))
+        .collect::<Vec<_>>()
+        .into_par_iter()
         .map(|(x, y)| {
             let b = xy_fn(x, y);
             if b != 0 {
