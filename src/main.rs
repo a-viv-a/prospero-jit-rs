c1293823ebae5a296b63246e414f58bb850730bc
Aviva Ruben <aviva@rubenfamily.com>
2025-03-24 13:09:20 -0500

    sideways but working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{fs::File, io::Write};
+
 use itertools::Itertools;
 use translate::translate;
 
@@ -12,12 +14,9 @@ fn main() {
     let step = 1f64 / dim as f64;
 
     let xy_fn = translate(src);
-    (0..dim)
+    let bytes = (0..dim)
         .cartesian_product(0..dim)
-        .map(|(x_step, y_step)| {
-            dbg!(x_step, y_step);
-            (-1f64 + step * x_step as f64, -1f64 + step * y_step as f64)
-        })
+        .map(|(x_step, y_step)| (-1f64 + step * x_step as f64, -1f64 + step * y_step as f64))
         .map(|(x, y)| {
             let b = xy_fn(x, y);
             if b != 0 {
@@ -25,5 +24,14 @@ fn main() {
             } else {
                 false
             }
-        });
+        })
+        .map(|b| match b {
+            true => 255u8,
+            false => 0u8,
+        })
+        .collect::<Vec<_>>();
+
+    let mut out = File::create("out.ppm").unwrap();
+    write!(out, "P5\n{dim} {dim}\n255\n").unwrap();
+    out.write(&bytes[..]).unwrap();
 }
